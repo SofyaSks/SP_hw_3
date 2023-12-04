4692c894fcd692a87110acce9e7f54fa3dbfa8b9
sks <ssurovneva@mail.ru>
2023-12-04 23:44:08 +0300

    Add project files.

---
+++ SP_hw_3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+class Program
+{
+   
+    static int length = 4;
+    private static int count;
+
+    static void CountCount()
+    {
+        for (int i = 0; i < 100000; i++)
+        {
+            count++;
+        }
+
+    }
+    static void Main(string[] args)
+    
+    {
+        Thread[] threads = new Thread[length];
+
+
+        for (int i = 0; i < length; i++)
+        {
+            threads[i] = new Thread(CountCount);
+            threads[i].Start();
+        }
+        Console.WriteLine(count);
+        
+    }
+}
